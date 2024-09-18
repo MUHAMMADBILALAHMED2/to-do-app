5ed2625547582795a82ec882e66b4eb0f8839ac4
muhammadbilalahmed2 <muhammadbilalahmed434@gmail.com>
2024-09-19 02:41:06 +0500

    created todo

--- main.js
+++ main.js
@@ -16,3 +16,48 @@ let addtodo = [];
 //           `;
 //     });
 //   }
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   addtodo.push(input.value);
+//   console.log(addtodo);
+//   renderScreen();
+//   input.value = "";
+// });
+
+// // delete button
+// function deleteBtn(index) {
+//   addtodo.splice(index,1);
+//   renderScreen();
+// }
+
+// function editBtn(index) {
+//     let newTodo = prompt("ENTER NEW TODO")
+//     if (newTodo = " ") {
+// alert('poka')
+//     } else {
+//         addtodo.splice(index , 1 , newTodo)
+//         renderScreen()
+//     }
+// }
+
+function renderScreen() {
+    list.innerHTML = "";
+    addtodo.map((item, index) => {
+      list.innerHTML += `
+            <li>${item}
+            <button onclick="deleteBtn(${index})">Delete</button>
+            <button onclick="editBtn(${index})">Edit</button>
+            </li>
+            `;
+    });
+  }
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addtodo.push(input.value);
+    console.log(addtodo);
+    renderScreen();
+    input.value = "";
+  });
+  
