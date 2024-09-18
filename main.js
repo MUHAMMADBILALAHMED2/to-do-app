895a9c50db88f3ecf7c39c104afa79f4542b359a
muhammadbilalahmed2 <muhammadbilalahmed434@gmail.com>
2024-09-19 02:38:52 +0500

    created todo

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ let list = document.querySelector("#list");
 
 // javscript varibale
 let addtodo = [];
+
+// function renderScreen() {
+//     list.innerHTML = ''
+//     addtodo.map((item, index) => {
+//       list.innerHTML += `
+//           <li>${item}
+//           <button onclick="deleteBtn(${index})">Delete</button>
+//           <button onclick="editBtn(${index})">Edit</button>
+//           </li>
+//           `;
+//     });
+//   }
